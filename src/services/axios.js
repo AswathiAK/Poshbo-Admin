26ddebce86c6ac459a4b03f0ece62ac1bdd71a3d
Aswathi A K <aswathiak.3@gmail.com>
2023-10-25 10:52:49 +0530

    header added admin token

--- src/services/axios.js
+++ src/services/axios.js
@@ -5,4 +5,17 @@ const instance = axios.create({
    withCredentials: true
 });
 
+instance.interceptors.request.use(
+    (config) => {
+    const adminToken = Cookies.get('adminToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${adminToken}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
 export default instance;
