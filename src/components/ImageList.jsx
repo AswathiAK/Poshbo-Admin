5cb118a790e3656fb8987ad0177930aec4053e7a
Aswathi A K <aswathiak.3@gmail.com>
2024-08-06 12:11:13 +0530

    code updated

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,29 +1,3 @@
-// import * as React from 'react';
-// import ImageList from '@mui/material/ImageList';
-// import ImageListItem from '@mui/material/ImageListItem';
-
-// export default function StandardImageList({ hotel }) {
-
-//   return (
-//     <ImageList sx={{ width: 500, height: 230 }} cols={3} rowHeight={164}>
-//       {hotel?.photos?.length>0 && hotel.photos.map((photo,index) => (
-//         <ImageListItem key={index}>
-//           <img
-//             // src={`${photo}?w=164&h=164&fit=crop&auto=format`}
-//             // srcSet={`${photo}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-//             src={photo}
-//             srcSet={photo}
-//             alt='photos'
-//             loading="lazy"
-//             style={{ width: '200px', height: '100px', objectFit: 'cover' }}
-//           />
-//         </ImageListItem>
-//       ))}
-//     </ImageList>
-//   );
-
-// }
-
 import React, { useState } from 'react'
 
 const StandardImageList = ({ hotel }) => {
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,22 +1,22 @@
 import axios from "axios";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 const instance = axios.create({
-  baseURL: 'https://www.mollacart.online',
-   withCredentials: true
+  baseURL: "https://poshbo-server.onrender.com",
+  withCredentials: true,
 });
 
 instance.interceptors.request.use(
-    (config) => {
-    const adminToken = Cookies.get('adminToken');
-      if (adminToken) {
-        config.headers.Authorization = `Bearer ${adminToken}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  (config) => {
+    const adminToken = Cookies.get("adminToken");
+    if (adminToken) {
+      config.headers.Authorization = `Bearer ${adminToken}`;
     }
-  );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
